d6b3dc40df65501ee9934a37a23048e8c99b699a
Lincoln Rebouças <lincoln.reboucas40@gmail.com>
2024-02-05 21:43:11 -0300

    some notes

--- aulas3.hs
+++ aulas3.hs
@@ -34,3 +34,55 @@ ignore _ [] = []
 ignore n (x:xs) 
     | n == 0 = x : ignore n xs
     | otherwise = ignore (n-1) xs
+
+
+--- Lista VI
+
+temp :: Float -> Float
+temp c = 9/5 * c + 32
+
+isPar :: Int -> Bool
+isPar n = (mod n 2 == 0)
+
+isImpar :: Int -> Bool
+isImpar n = (mod n 2 == 1)
+
+maior :: [Int] -> Int
+maior [] = -1
+maior (x:xs)
+    | x >= maior xs = x
+    | otherwise = maior xs
+
+func :: Int -> [Int] -> [Int]
+func _ [] = []
+func n lista = filter (>n) lista
+
+func1 :: [Int] -> [Int]
+func1 [] = []
+func1 (x:xs) = x:x:func1 xs
+
+func2 :: Int -> Bool
+func2 n = length [x | x <- [1..n], mod n x == 0] == 2
+
+numerosPar :: [Int] -> [Int]
+numerosPar [] = []
+numerosPar (x:xs) = filter (isPar) (x:xs)
+
+func3 :: [Int] -> Int
+func3 [] = 0
+func3 (x:xs) 
+    | isImpar x = x^2 + func3 xs
+    | otherwise = func3 xs
+
+func4 :: [Int] -> Int
+func4 [] = 0
+func4 (x:xs) = foldr (\n acc -> if odd n then n*n + acc else acc) 0 (x:xs)
+
+func5 :: [Int] -> Int
+func5 [] = 0
+func5 (x:xs) = foldr1 (*) (x:xs)
+
+
+func6 :: [String] -> [Int]
+func6 [] = []
+func6 lista = map (length) (lista)
